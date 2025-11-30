b42aa6b7df6a3eb598beca75723c0c33a7ea8919
Misaka-L <lipww1234@foxmail.com>
2025-11-30 16:48:39 +0800

    feat: patches enabled settings

---
+++ src/Packages/xya.misakal.vpm.yes-patch-framework-for-sdk/Framework/Settings/PatchManager/YesPatchManagerPatchSettings.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace YesPatchFrameworkForVRChatSdk.Settings.PatchManager;
+
+[Serializable]
+internal sealed class YesPatchManagerPatchSettings
+{
+    public YesPatchManagerPatchSettings(string id, bool isEnabled)
+    {
+        Id = id;
+        IsEnabled = isEnabled;
+    }
+
+    public string Id { get; set; }
+    public bool IsEnabled { get; set; }
+}
---
+++ src/Packages/xya.misakal.vpm.yes-patch-framework-for-sdk/Framework/Settings/PatchManager/YesPatchManagerSettings.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+// DO NOT USE file scoped namespace, It will brake ScriptableObject creation
+namespace YesPatchFrameworkForVRChatSdk.Settings.PatchManager
+{
+    internal sealed class YesPatchManagerSettings : ScriptableObject
+    {
+        public const string FolderName = "YesPatchFrameworkForVRChatSdk";
+        public const string FolderPath = "Assets/" + FolderName;
+        public const string AssetPath = "Assets/" + FolderName + "/" + "YesPatchManagerSettings.asset";
+
+        [SerializeField] private List<YesPatchManagerPatchSettings> patchSettings = new();
+
+        internal static YesPatchManagerSettings GetOrCreateSettings()
+        {
+            var settings = AssetDatabase.LoadAssetAtPath<YesPatchManagerSettings>(AssetPath);
+            if (settings != null)
+                return settings;
+
+            settings = CreateInstance<YesPatchManagerSettings>();
+
+            if (!AssetDatabase.IsValidFolder(FolderPath))
+                AssetDatabase.CreateFolder("Assets", FolderName);
+
+            AssetDatabase.CreateAsset(settings, AssetPath);
+            AssetDatabase.SaveAssets();
+            return settings;
+        }
+
+        public bool IsPatchEnabled(string patchId)
+        {
+            var setting = patchSettings.Find(s => s.Id == patchId);
+            return setting?.IsEnabled ?? true;
+        }
+
+        public void SetPatchEnabled(string patchId, bool isEnabled)
+        {
+            var setting = patchSettings.Find(s => s.Id == patchId);
+            if (setting != null)
+            {
+                setting.IsEnabled = isEnabled;
+            }
+            else
+            {
+                patchSettings.Add(new YesPatchManagerPatchSettings(patchId, isEnabled));
+            }
+
+            EditorUtility.SetDirty(this);
+            AssetDatabase.SaveAssets();
+        }
+    }
+}
