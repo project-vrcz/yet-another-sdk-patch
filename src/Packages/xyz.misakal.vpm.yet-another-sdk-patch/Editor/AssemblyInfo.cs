66f1e22ff22bea6e725ec362010f63c90b155575
Misaka-L <lipww1234@foxmail.com>
2025-12-06 21:37:54 +0800

    feat: randomize device id

--- src/Packages/xyz.misakal.vpm.yet-another-sdk-patch/Editor/AssemblyInfo.cs
+++ src/Packages/xyz.misakal.vpm.yet-another-sdk-patch/Editor/AssemblyInfo.cs
@@ -2,6 +2,7 @@
 using YetAnotherPatchForVRChatSdk.Patches;
 using YetAnotherPatchForVRChatSdk.Patches.NetworkResilience;
 
+[assembly: ExportYesPatch(typeof(RandomizeDeviceIdPatch))]
 [assembly: ExportYesPatch(typeof(NetworkResiliencePatch))]
 [assembly: ExportYesPatch(typeof(RemoteConfigCachePatch))]
 [assembly: ExportYesPatch(typeof(AlwaysAgreeCopyrightAgreementPatch))]
---
+++ src/Packages/xyz.misakal.vpm.yet-another-sdk-patch/Editor/Patches/RandomizeDeviceIdPatch.cs
@@ -0,0 +1,36 @@
+﻿using HarmonyLib;
+using UnityEngine;
+using VRC.Core;
+using YesPatchFrameworkForVRChatSdk.PatchApi;
+
+namespace YetAnotherPatchForVRChatSdk.Patches;
+
+internal sealed class RandomizeDeviceIdPatch : YesPatchBase
+{
+    public override string Id => "xyz.misakal.vpm.yet-another-sdk-patch.randomize-device-id-patch";
+    public override string DisplayName => "Randomize Device ID";
+    public override string Description => "Randomizes the device ID sent with API requests.";
+
+    public override string Category => "Privacy?";
+
+    private readonly Harmony _harmony = new("xyz.misakal.vpm.yet-another-sdk-patch.randomize-device-id-patch");
+
+    public override void Patch()
+    {
+        _harmony.PatchAll(typeof(RandomizeDeviceIdPatch));
+    }
+
+    public override void UnPatch()
+    {
+        _harmony.UnpatchSelf();
+    }
+
+    [HarmonyPatch(typeof(API), nameof(API.DeviceID), MethodType.Getter)]
+    [HarmonyPrefix]
+    private static bool RandomizeDeviceIdPrefix(ref string __result)
+    {
+        // Generate a random device ID
+        __result = System.Guid.NewGuid().ToString();
+        return false; // Skip original method
+    }
+}
